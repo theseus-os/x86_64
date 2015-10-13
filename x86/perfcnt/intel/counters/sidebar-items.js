e6a7e28852e987e18bf4d80fb0af13f735831213
Gerd Zellweger <mail@gerdzellweger.com>
2015-10-13 10:34:56 +0100

    Update documentation

---
+++ x86/perfcnt/intel/counters/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"static":[["COUNTER_MAP",""]]});
---
+++ x86/perfcnt/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["available_counters","Return performance counter description for the running micro-architecture."]],"mod":[["intel",""]]});
